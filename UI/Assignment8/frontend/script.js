6ec5d1ec10d03ea8a2ddeaaab5f0576436278be0
meta-kunal-kaushik <59638400+meta-kunal-kaushik@users.noreply.github.com>
2020-03-16 23:38:56 +0530

    Add files via upload

---
+++ UI/Assignment8/frontend/script.js
@@ -0,0 +1,15 @@
+class Node {
+    constructor(value) {
+        this.element = element;
+        this.nextLink = null;
+    }
+}
+class LinkList {
+    constructor() {
+        this.header = null;
+    }
+
+    add(element) {
+
+    }
+}
